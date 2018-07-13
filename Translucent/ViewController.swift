76ab0bf3f366dd996553562b956f3bf119e312a6
Shi Liu <liushie@gmail.com>
2018-07-13 19:05:05 +0800

    Initial Commit

---
+++ Translucent/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Translucent
+//
+//  Created by Shi on 2018/7/13.
+//  Copyright © 2018年 Shi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
