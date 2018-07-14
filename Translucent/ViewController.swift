3cbbd78ea4934aa98d8403fda3cb33a80c6b887d
Shi Liu <liushie@gmail.com>
2018-07-14 15:11:38 +0800

    introducing the issue

--- Translucent/ViewController.swift
+++ Translucent/ViewController.swift
@@ -10,10 +10,41 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    
+    @IBOutlet weak var scrollView: NSScrollView!
+    @IBOutlet weak var clipView: NSClipView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let path = "/Library/Desktop Pictures/Snow.jpg"
+        let url = URL(fileURLWithPath: path, isDirectory: false)
+        
+        let image = NSImage(contentsOf: url)
+        
+        let imageView = NSImageView(image: image!)
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        clipView.addSubview(imageView)
+        
+        let h = NSLayoutConstraint.constraints(
+            withVisualFormat: "H:|[imageView]|",
+            options: NSLayoutConstraint.FormatOptions(rawValue: 0),
+            metrics: nil,
+            views: ["imageView" : imageView])
+        
+        let v = NSLayoutConstraint.constraints(
+            withVisualFormat: "V:|[imageView]|",
+            options: NSLayoutConstraint.FormatOptions(rawValue: 0),
+            metrics: nil,
+            views: ["imageView" : imageView])
+        
+        
+        scrollView.documentView = imageView
+        
+        NSLayoutConstraint.activate([h, v].flatMap({$0}))
+        
     }
 
     override var representedObject: Any? {
